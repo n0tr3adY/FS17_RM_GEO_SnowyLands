10b43d1b2131b2ed96307d2915425a9d72c00a70
Jos Kuijpers <jkuijpers@jarvix.nl>
2017-06-19 21:08:07 +0200

    Rename to Snowy Lands

--- src/loader.lua
+++ src/loader.lua
@@ -12,5 +12,22 @@ geoMod.modDir = g_currentModDirectory
 
 function g_rm_seasons_load(seasons)
     -- Load data from data/ folder
-    g_seasons:registerXMLDirectory("antarctica", geoMod.modDir .. "data/")
+    g_seasons:registerXMLDirectory("snowylands", geoMod.modDir .. "data/")
 end
+
+function geoMod:setup()
+    g_seasons.environment.calculateVisualSeason = Utils.overwrittenFunction(g_seasons.environment.calculateVisualSeason, geoMod.calculateVisualSeason)
+    g_seasons.environment.latestVisualSeason = g_seasons.environment.SEASON_WINTER
+
+    for _, listener in ipairs(g_seasons.environment.visualSeasonChangeListeners) do
+        listener:visualSeasonChanged(newVisuals)
+    end
+end
+
+function geoMod:calculateVisualSeason(superFunc)
+    superFunc(self)
+
+    return g_seasons.environment.SEASON_WINTER
+end
+
+Mission00.loadMission00Finished = Utils.appendedFunction(Mission00.loadMission00Finished, geoMod.setup)
